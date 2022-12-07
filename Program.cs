33175118ecb9dc9f76a5383d537ad3062e10fb7d
garaevo3 <5mptrbf@code.edu.az>
2022-12-08 00:38:30 +0400

    done

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text.Json.Nodes;
+
+namespace hometask_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsons;
+            using (WebClient wc = new WebClient())
+            {
+               jsons = wc.DownloadString("https://jsonplaceholder.typicode.com/posts");
+            }
+            using(StreamWriter sw = new StreamWriter(@"C:\\Users\\samir\\Desktop\\codeAcademy\\backend\\hometask-13\\myJson.json"))
+            {
+                sw.Write(jsons);
+            }
+        }
+    }
+}
